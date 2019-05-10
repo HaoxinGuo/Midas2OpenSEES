eedd564543db1c5c35a60fd7ca1da6aee542b0f8
Guohaoxin <1210188858@qq.com>
2019-05-10 14:17:36 +0800

    加入视图文件model_view

--- Midas2OpenSees.py
+++ Midas2OpenSees.py
@@ -7,66 +7,6 @@ Created on Tue May  7 18:56:57 2019
 
 import os
 
-
-def flatten_tcl(tclfiles):
-    ''' This function takes a tcl file and rewrites it to a temporary file.
-    The new temporary file will be flattened, meaning all expressions are
-    replaced with their evaluated value (as a float)
-    '''
-    variables = {} # a disctionary of variables set in the tcl file
-    tempfiles = [] # a list of the temporary files created
-    ns = {'__builtins__':None} # create empty namespace to use eval() safely
-
-    for tclfile in tclfiles:
-        tempfiles.append(tclfile[:-4]+'_temp.tcl')
-
-
-        with open(tclfile) as f_in, open(tempfiles[-1], 'w') as f_out:
-            for line in f_in:
-                # For lines that define variable, add that variable to dictionary
-                if line[:3] == 'set':
-                    # If the line has 3 words, the variable is set directly and we
-                    #  can add it to our dictionary of variable values
-                    if len(line.split()) == 3:
-                        variables['$'+line.split()[1]] = float(line.split()[2])
-                        f_out.write(line)
-                    # If the thrid word is an expression, evaluate before write
-                    elif '[expr' in line.split()[2]:
-                        expr = line[line.find('[expr ')+6:line.find(']')]
-                        for variable in variables:
-                            if variable in expr:
-                                # If variables in expr, replace with value
-                                expr = expr.replace(variable,
-                                                    str(variables[variable]))
-                        # Replace expression with evaluated value of expression
-                        expr = eval(expr, ns)
-                        variables['$'+line.split()[1]] = float(expr)
-                        f_out.write(' '.join(('set', line.split()[1], str(expr))))
-                # For lines that don't define variable, eval expressions and print
-                else:
-                    for variable in variables:
-                        line = line.replace(variable, str(variables[variable]))
-                    while '[expr' in line: # evaluate any expressions, then print
-                        expr = line[line.find('[expr ')+6:line.find(']')]
-                        line = line.replace('[expr '+expr+']', str(eval(expr, ns)))
-                    f_out.write(line)
-
-    # Combine all _temp tcl files into one messy one, also detect for 3D of 2D
-    ndm = 2 # assume 3 dimensions unless '-ndm 2' detected
-    with open('temp.tcl', 'w') as f:
-        for tempfile in tempfiles:
-            with open(tempfile, 'r') as temp:
-                for line in temp:
-                    f.write(line)
-                    if ndm == 2 and '-ndm 3' in line:
-                        ndm = 3
-                    if ndm == 3 and '-ndm 2' in line:
-                        ndm = 'Uh oh, we\'re not sure which ndm to use...'
-            os.remove(tempfile)
-
-    return 'temp.tcl', ndm
-
-
 def getNodeEleSec(datas):
     '''
     函数getNodeEleSec(datas) 定义得到节点 单元 截面信息 输入文件为datas 即去掉，号的mct文件
@@ -198,7 +138,7 @@ def WriteFunc(nodes,elements,SecValue,Secnum,fix):
                         + '\n' )  
             
 # 输入文件即mct文件
-tclfiles = ('11.tcl',)
+# tclfiles = ('11.tcl',)
 
 # 自输入材料值
 poisson = 0.20
@@ -206,8 +146,11 @@ E1 = 34.5e9
 G1 = E1/(2*(1 + poisson))
 
 # 文件处理
-
-tclfile, ndm = flatten_tcl(tclfiles)
+# print('Please Enter the file name')
+tclfile = input("输入文件名:")
+#with open(tclfile) as f_in:
+    
+# tclfile, ndm = flatten_tcl(tclfiles)
 
 ndm = 3
 
